de02235b0d3cea4f8beefa87c1c5f6348bdb5ca4
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2021-02-21 12:58:17 -0300

    feat: initial commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="Card">
+      <p>Im spinning.</p>
+    </div>
+  );
+}
+
+export default Card;
--- src/Card.test.js
+++ src/Card.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Card from './Card';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Card />);
+  const linkElement = screen.getByText(/Im spinning/i);
   expect(linkElement).toBeInTheDocument();
 });
